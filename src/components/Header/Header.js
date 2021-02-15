e25607ab9456993fa55deaa113c869cb99d93744
Ruslan Yusupov <1renegate1@gmail.com>
2021-02-15 14:18:41 +0500

    fix mobile layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import mainImage from "../../images/main-img.png"
 function Header() {
     return(
         <header>
-            <h1>myNotes<img alt="Yellow Post it" src={mainImage}></img></h1>
+            <h1>myNotes<img className="img-in-header" alt="Yellow Post it" src={mainImage}></img></h1>
         </header>
         
     )
